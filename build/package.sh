bf207a574014ba0609bd8da8ed29f2f5e35a38de
guokai <han.guokai@gmail.com>
2022-05-21 20:42:07 +0800

    update build for new dir structure

--- build/package.sh
+++ build/package.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-file="../background.js"
+rootDir="manifest-v3" # or "manifest-v2"
+cd ..
+
+file="${rootDir}/background.js"
 mv "$file" "$file".bak
-java -jar $CLOSURE_JAR --language_out ECMASCRIPT_2018 --js_output_file="$file" "$file".bak
+java -jar $CLOSURE_JAR --language_out ECMASCRIPT_2020 --js_output_file="$file" "$file".bak
 
 # package
-cd ../..
-zip -r9 mdn-search.zip mdn-search/ -x mdn-search/build/\* mdn-search/webstore/\* mdn-search/.git/\* \*.DS_Store \*.bak \*README.md \*LICENSE
+zip -r9 mdn-search.zip  "$rootDir/" -x \*.DS_Store \*.bak
 
 # after package
-cd mdn-search/build
 rm "$file"
 mv "$file".bak "$file"
+cd ./build
